180e01fb0a02529fb4ba6fed92335a1884dce7f1
Andreas Hammerbeck <andreas_93@hotmail.com>
2017-10-03 11:46:01 +0200

    Search and datatableview on auctions

--- AuctionHouse/src/java/my/presentation/HomeView.java
+++ AuctionHouse/src/java/my/presentation/HomeView.java
@@ -7,25 +7,63 @@ package my.presentation;
 
 import boundary.AuctionFacade;
 import entities.Auction;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.inject.Named;
-import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 /**
  *
  * @author addi2
  */
-@Named(value = "homeView")
-@RequestScoped
+@ManagedBean(name="homeView")
+@ViewScoped
 public class HomeView {
-     @EJB
+    @EJB
     AuctionFacade auctionFacade;
+    List<Auction> auctions;
+    List<Auction> filteredAuctions;
+    String searchText;
 
     /**
      * Creates a new instance of HomeView
      */
     public HomeView() {
+    }
+    @PostConstruct
+    public void init(){
+        getAllAuctions();
+        searchText="";
+    }
+    public List<Auction> getAuctions() {
+        return auctions;
+    }
+    public void search(){
+        getAllAuctions();
+        filteredAuctions =  auctions.stream().filter((a) -> (a.getAuctionName().toUpperCase().contains(searchText.toUpperCase()))).collect(Collectors.toList());
+    }
+
+    public List<Auction> getFilteredAuctions() {
+        return filteredAuctions;
+    }
+
+    public void setFilteredAuctions(List<Auction> filteredAuctions) {
+        this.filteredAuctions = filteredAuctions;
+    }
+
+    public void setAuctions(List<Auction> auctions) {
+        this.auctions = auctions;
+    }
+
+    public String getSearchText() {
+        return searchText;
+    }
+
+    public void setSearchText(String searchText) {
+        this.searchText = searchText;
     }
     
     
@@ -35,8 +73,13 @@ public class HomeView {
     }
     
       // Returns all auctions
-    public List<Auction> getAllAuctions(){
-       return auctionFacade.findAllAuctions();
+    public void getAllAuctions(){
+        try{
+            auctions =  auctionFacade.findAllAuctions();
+        }catch(Exception e){
+            auctions = new ArrayList();
+        }
+ 
     }
     
     public String goToHome(){
