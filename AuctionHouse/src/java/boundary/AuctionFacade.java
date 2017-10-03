91186267adc0573d126c103e6927e6aa75eb4fe3
Andreas Hammerbeck <andreas_93@hotmail.com>
2017-10-03 20:05:26 +0200

    Fix set auction to finished when time runs out

--- AuctionHouse/src/java/boundary/AuctionFacade.java
+++ AuctionHouse/src/java/boundary/AuctionFacade.java
@@ -36,14 +36,14 @@ public class AuctionFacade extends AbstractFacade<Auction> {
     returns all auctions by a specific user queryed by id
     */
     public List<Auction> findYourAuctions(long ownerId) {
-        Query query = em.createQuery("SELECT a FROM Auction a WHERE CAST(a.auctionOwner.id AS VARCHAR(10)) LIKE CAST(?1 AS VARCHAR(10))");
+        Query query = em.createQuery("SELECT a FROM Auction a WHERE CAST(a.auctionOwner.id AS VARCHAR(10)) LIKE CAST(?1 AS VARCHAR(10)) AND a.finished = false");
         query.setParameter(1, ownerId);
         List<Auction> list= query.getResultList();
         return  list;
     }
         public List<Auction> findYourPurchases(long ownerId) {
             //change to bidOwner
-        Query query = em.createQuery("SELECT a FROM Auction a WHERE CAST(a.auctionOwner.id AS VARCHAR(10)) LIKE CAST(?1 AS VARCHAR(10))");
+        Query query = em.createQuery("SELECT a FROM Auction a WHERE CAST(a.auctionOwner.id AS VARCHAR(10)) LIKE CAST(?1 AS VARCHAR(10)) AND a.finished = true");
         query.setParameter(1, ownerId);
         List<Auction> list= query.getResultList();
         return  list;
@@ -55,6 +55,8 @@ public class AuctionFacade extends AbstractFacade<Auction> {
     }
     public void setFinished(long auctionId){
         Query query = em.createQuery("UPDATE Auction a SET a.finished = true, a.status = false  WHERE CAST(a.id AS VARCHAR(10)) LIKE CAST(?1 AS VARCHAR(10))" );
+        query.setParameter(1, auctionId);
+        query.executeUpdate();
     }
     
  
